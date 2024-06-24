de32559f763f0dbf075b4f2fa8fb357385dbc9dd
Colin Craig <73488952+ccraig17@users.noreply.github.com>
2024-06-23 22:32:39 -0400

    Created the ProfileActivity.java as well as the activity_profile.xml

--- app/src/main/java/com/craig/mychatapp/SignUpActivity.java
+++ app/src/main/java/com/craig/mychatapp/SignUpActivity.java
@@ -63,6 +63,8 @@ public class SignUpActivity extends AppCompatActivity {
         firebaseStorage = FirebaseStorage.getInstance();
         storageReference = firebaseStorage.getReference();
 
+
+
         registerActivityForSelectImage();
 
         imageViewCircle.setOnClickListener((v -> {
